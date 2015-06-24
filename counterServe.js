93aa4937ca45b2df2674ec4db861b61eba242d4f
Mike Mostachetti <mjmostachetti@gmail.com>
2015-06-24 11:52:33 -0700

    server adding unique id to all initial create/posts

--- counterServe.js
+++ counterServe.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
 	var Counter = Backbone.Model.extend({
 		defaults : {"value":0},
-		urlRoot:"/counter",
-
+		urlRoot:"/counter"
 	});
 
 	var Texter = Backbone.Model.extend({
-		defautls : {"text":""},
-		urlRoot: "/text"
+		defaults : {
+			"text":"",
+			"newText" : ""
+		},
+		urlRoot: "/text",
+		concat : function(){
+			var textz = this.get('text')
+			var newTextz = this.get("newText")
+			this.set("text",textz+newTextz)
+			this.save()
+		}
 	})
 
 	var counterModel1 = new Counter({ id : 1});
+	var counterModel2 = new Counter;
 	var texterModel = new Texter({ id : 1 })
 
 	Counter.prototype.inc = function(){
@@ -26,6 +35,8 @@ $(document).ready(function(){
 	}
 
 	counterModel1.fetch();
+	counterModel2.fetch();
+
 	var syncNum = 0;
 	var CounterView = Backbone.View.extend({
 		render: function(){
@@ -56,10 +67,32 @@ $(document).ready(function(){
 		}
 	});
 
+	var textView = Backbone.View.extend({
+		render : function(){
+			this.$el.html('<input type="text" value="textVal"><button>')
+		},
+		initialize : function(){
+			this.listenTo(this.model, 'sync', this.concat)
+		},
+		events : {
+			'click button' : 'adder'
+		},
+		adder : function(){
+			this.model.set('newText',this.$el.find('input').val())
+			this.model.concat()
+		}
+	})
+
 	var counterView1 = new CounterView({ model : counterModel1 })
+	var counterView2 = new CounterView({ model : counterModel2 })
+	var textView1 = new textView({ model : texterModel })
 
 	counterView1.render()
+	counterView2.render()
+	textView1.render()
+
 	console.log("counterView1.$el is : " + counterView1.$el)
 	$("#counterdiv").append(counterView1.$el)
-
+	$("#counterdiv").append(counterView2.$el)
+	$("#textdiv").append(textView1.$el)
 })
--- counterServer.js
+++ counterServer.js
@@ -4,22 +4,39 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var counter1 = 0;
+var counter2 = 0;
+var nextId = 2;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
 
-app.get('/counter/1', function (req, res) {
+app.get('/counter/:val', function (req, res) {
+    var num = req.params.val
+    console.log("This is the params passed in :" + num)
     console.log("counter has been requested");
     res.send(JSON.stringify({value : counter1}));
 });
 
-app.put('/counter/1', function (req, res) {
+
+app.put('/counter/:val', function (req, res) {
     console.log(req.body);
-    counter1 = req.body.value;
     var json = JSON.stringify({})
     res.end(json);
 });
+
+app.post('/counter',function (req,res){
+    console.log('server side post')
+    var json = JSON.stringify({id:nextId})
+    res.end(json);
+    nextId++;
+});
+
+app.get('/counter', function (req,res){
+
+})
+
+//app.get('/text')
 
 app.listen(3000, function () {
     console.log("server started");
